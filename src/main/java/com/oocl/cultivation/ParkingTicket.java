6971880061959a54c44f421362c50da00d54ba0e
HOJO3 <joe.ho@oocl.com>
2018-11-27 00:26:01 +0800

    refactor car availability field

--- src/main/java/com/oocl/cultivation/ParkingTicket.java
+++ src/main/java/com/oocl/cultivation/ParkingTicket.java
@@ -2,7 +2,7 @@ package com.oocl.cultivation;
 
 public class ParkingTicket {
     private Car car;
-    public Boolean availability = true;
+    private Boolean availability = true;
 
     public ParkingTicket (Car car){this.car = car;}
     public ParkingTicket (){}
